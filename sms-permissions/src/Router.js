f79548de62dacff16a1c9252099b1f8e40dde5da
Jason Aviles <sandman2k18@outlook.com>
2019-07-03 02:18:14 -0400

    update working

--- sms-permissions/src/Router.js
+++ sms-permissions/src/Router.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import {Route} from 'react-router-dom'
 import Home from './components/Home'
@@ -7,23 +7,40 @@ import Login from './components/login'
 import Register from './components/Register'
 import Footer from './components/footer'
 import Header from './components/Header'
-export default function ButtonAppBar() {
+import AdminHome from './components/AdminPages/AdminHome'
+
+
+
+
+class Router extends Component {
+
+
+
+
+
+
+
   
-
-  return (
-    <div style={{flexGrow: 1 ,
-      fontSize: '2rem',background:'#200835', display:"flex" ,flexDirection:'column',justifyContent:'center' ,alignItems:'center'}}>
-     <Header/>
-      <div style={{background:'#FFFFFF' ,width:'50%'}}>
-<Route exact path='/'   component={Home}/>
-
-
-<Route exact path='/about_Us'   component={About_us}/>
-<Route exact path='/register'   component={Register}/>
-<Route exact path='/login'   component={Login}/></div>
-
-
-<Footer/>
-    </div>
-  );
+  render() {
+    return (
+      <div style={{flexGrow: 1 ,
+        fontSize: '2rem',background:'#34004B', display:"flex" ,flexDirection:'column',justifyContent:'center' ,alignItems:'center'}}>
+       
+        <Route  path='/' component={Header}  />
+        <Route  path='/admin' component={AdminHome}  />
+        <div style={{background:'#FFFFFF' ,width:'50%'}}>
+  <Route exact path='/'   component={Home}/>
+  
+  
+  <Route exact path='/about_Us'   component={About_us}/>
+  <Route exact path='/register'   component={Register}/>
+  <Route exact path='/login'   component={Login}/></div>
+  
+  
+  <Footer/>
+      </div>
+    );
+  }
 }
+
+export default Router;
--- sms-permissions/src/components/AdminForms/AddStudentsToClass.js
+++ sms-permissions/src/components/AdminForms/AddStudentsToClass.js
@@ -1,18 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AddStudentsToClass extends Component {
-    render() {
-        return (
-            <div>
-                <form>
-                    <input type="text" name="studentFullName" placeholder="Student Name" />
-                    <input type="text" name="parentName" placeholder="Parent Name" />
-                    <input type="tel" name="contactNumber" placeholder="555-555-5555"/>
-                    <button>+Add Student</button>
-                </form>
-            </div>
-        );
+  state = {
+        
+    student: {
+    name: '',
+    teacher_id:''
+    },
+    guardian: {
+    name: "",
+    contact: ""
     }
+    
+  }
+
+
+axios
+
+  addStudent = event => {
+    event.preventDefault();
+    // add code to create
+    this.props.addStudent(this.state);
+    this.setState({
+        
+    student: {
+    name: '',
+    teacher_id:''
+    },
+    guardian: {
+    name: " ",
+    contact: " "
+    }
+    
+  });
+  };
+
+  handleInputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+      console.log(this.state.student.name)
+    return (
+      <div>
+        <form onSubmit={this.addStudent}>
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            name='name'
+            placeholder="Student Name"
+            value={this.state.student.name}
+          />
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            name='StudentTeacher'
+            placeholder="Parent Name"
+            value={this.state.student.teacher_id}
+          />
+          <input
+            onChange={this.handleInputChange}
+            type="text"
+            name='guardianName'
+            placeholder="name"
+            value={this.state.guardian.name}
+          />
+
+<input
+            onChange={this.handleInputChange}
+            type="tel"
+            value={this.state.guardian.contact}
+            name='contact'
+            placeholder="555-555-5555"
+          />
+          <button>+Add Student</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default AddStudentsToClass;
