2f4ac2adb9d7fea7e51aea819f8df5ef5603b26c
curtdragon <tealturboser@gmail.com>
2019-07-03 02:34:48 -0500

    About JS and CSS. Deleted app.css (not being used). Added reset to index.css. Updated register and signin js and login.css to make it DRY

--- sms-permissions/src/components/login.js
+++ sms-permissions/src/components/login.js
@@ -4,20 +4,20 @@ import picture from "./images/signuppage/teacher.jpg";
 
 const login = () => {
     return (
-        <div style={{ display: 'flex', width: '100%', height: "auto" }}>
-            <div style={{ background: "#F5F5F5", width: '50%', paddingBottom: "55%"}} className="description">
+        <div className="login">
+            <div className="description">
                 <p>Contact Parents or Guardians with a quick secure response for permission slips.</p>
-                <img src={picture} alt="teacher" style={{ width:"90%", height: "150px"}}/>
+                <img src={picture} alt="teacher"/>
             </div>
             
-            <div style={{ background: "#34004B", width: '50%', flexDirection: 'column', paddingBottom: "55%", paddingTop: "35%"}} className="signup">
+            <div className="signup">
                 <h1>Sign in</h1>
-                <form style={{ background: "#34004B", width: '50%', flexDirection: 'column' }} className="signin">
+                <form className="signin">
                     <label className="label">EMAIL</label>
                     <input className="input" type="email" name="email" placeholder="Email address" />
                     <label className="label">PASSWORD</label>
                     <input className="input" type="password" placeholder="***********" name="password" />
-                    <div style={{fontSize: ".6rem"}} className="submit">
+                    <div className="submit">
                         <button type="submit">Sign In</button>
                         <p>Need to Sign up?</p>
                     </div>
